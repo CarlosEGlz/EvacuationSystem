f9bda0c197943857e00382905bb06dfcb396ce65
CarlosEGlz <107332385+CarlosEGlz@users.noreply.github.com>
2023-04-16 10:01:43 -0500

    Create House.h

---
+++ House.h
@@ -0,0 +1,36 @@
+#pragma
+#include <map>
+#include <vector>
+
+class House
+{
+private:
+    int road;
+    int section;
+    int number_occupants;
+    int number_of_cars;
+    std::multimap<int, int> traversedMap;
+    std::vector<int> roadVect;
+    std::vector<int> sectVect;
+
+public:
+    //(this->) get the CLASS private variable
+    House();
+    House(int road, int section, int number_occupants,
+          int number_of_cars, std::multimap<int, int> traversedMap,
+          std::vector<int> roadVect, std::vector<int> sectVect);
+
+    void validate();
+    void setRoad(int road);
+    void setSection(int section);
+    void setOccupants(int number_occupants);
+    void setNumOfCars(int number_of_cars);
+    void traversedRoadSection(int NUM_OF_HOUSES, int TIME);
+
+    int getRoad();
+    int getSection();
+    int getOccupants();
+    int getNumOfCars();
+
+    void print();
+};
